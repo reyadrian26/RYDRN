144d88180027b62c10e4caf024808bbf5a9d8890
Rey Adrian Inday <72378242+reyadrian26@users.noreply.github.com>
2024-02-18 18:36:15 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
+const sr = ScrollReveal ({
+    distance: '10px',
+    duration: 2500,
+    reset: true
+})
+
+sr.reveal('.home-text', {delay: 200, origin:'top'})
+sr.reveal('.home-img', {delay: 200, origin:'top'})
+sr.reveal('.about, .resume, .portfolio, .contact', {delay: 200, origin:'top'})
+
+
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   x[slideIndex-1].style.display = "block";  
+// }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function sendEmail() {
+    var name = document.getElementById('nameInput').value;
+    var email = document.getElementById('emailInput').value;
+    var message = document.getElementById('messageInput').value;
+
+    var mailtoLink = 'mailto:indayreyadrian@gmail.com?subject=Message from ' + name + '&body=' + encodeURIComponent(message + '\n\nSender Email: ' + email);
+
+    window.location.href = mailtoLink;
+}
